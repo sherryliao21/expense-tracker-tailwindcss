852de9da19a9cf8ffb0eada7b9572dde48677669
Sherry <sherry.c.liao@gmail.com>
2021-02-03 22:01:28 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const routes = require('./routes')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const PORT = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({
   defaultLayout: 'main', extname: '.hbs', helpers: {
@@ -18,6 +19,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log(`App is running on localhost:3000`)
 })
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/Expense', { useNewUrlParser: true, useUnifiedTopology: true })
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/Expense'
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 
 db.on('error', () => {
