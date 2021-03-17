b77154da888c8de8a36b42f9f63527402da6721a
Sherry <sherry.c.liao@gmail.com>
2021-03-17 19:06:44 +0800

    feat: add user model

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  confirmPassword: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('User', userSchema)
