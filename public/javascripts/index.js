4c0f1a6d641259933d65ddcb4ba3fed9f61a2e47
Sherry <sherry.c.liao@gmail.com>
2021-03-19 01:30:09 +0800

    feat: add reset month && category button

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,3 +2,13 @@ function confirmDelete() {
   if (confirm('確定要刪除這間餐廳？')) return true
   return false
 }
+
+const resetBtn = document.querySelector('.reset-btn')
+const monthForm = document.querySelector('.form-select-month')
+const categoryForm = document.querySelector('.form-category')
+resetBtn.addEventListener('click', (e) => {
+  if (e.target === resetBtn) {
+    categoryForm.value = 'all'
+    monthForm.value = 'all'
+  }
+})
