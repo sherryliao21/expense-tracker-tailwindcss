65c4993e19a161256e92bffbc1fec00932f19800
Sherry <sherry.c.liao@gmail.com>
2021-04-03 17:43:39 +0800

    feat: add label to form selects

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-const Category = require('../../models/category')
 const Record = require('../../models/record')
 
 router.get('/', (req, res) => {
