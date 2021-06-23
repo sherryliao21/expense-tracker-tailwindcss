7b74a72a1505fca8d971cfb728a808773a9ce21d
sherryliao21 <sherry.c.liao@gmail.com>
2021-06-24 00:05:29 +0800

    refactor home.js & index.hbs to async/await style and rename variables

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,49 +1,51 @@
 const express = require('express')
 const router = express.Router()
-
 const Record = require('../../models/record')
 
-router.get('/', (req, res) => {
-  const userId = req.user._id
-  const category = req.query.category
-  const yearMonth = req.query.yearMonth
-  const monthOptions = new Set();
-  const categoryOptions = new Set()
+router.get('/', async(req, res) => {
+  try {
+    const userId = req.user._id
+    const category = req.query.category
+    const date = req.query.date
+    const monthOptions = new Set();
+    const categoryOptions = new Set()
 
-  let filterCondition = { userId }
-  if (yearMonth && yearMonth !== 'all') {
-    const [year, month] = yearMonth.split('-')
-    const startDate = new Date(year, month - 1)
-    const endDate = new Date(year, month, 0)
-    filterCondition = {
-      ...filterCondition,
-      date: {
-        $gte: startDate.toISOString().split('T')[0],
-        $lt: endDate.toISOString().split('T')[0]
+    // create filterCondition object to store query filters
+    let filterCondition = { userId }  // first pass in userId
+    // add month filter
+    if (date && date !== 'all') {
+      const [year, month] = date.split('-')
+      const startDate = new Date(year, month - 1)
+      const endDate = new Date(year, month, 0)
+      filterCondition = {
+        ...filterCondition,
+        date: {
+          $gte: startDate.toISOString().split('T')[0],
+          $lt: endDate.toISOString().split('T')[0]
+        }
       }
     }
-  }
-  if (category && category !== 'all') {
-    filterCondition = {
-      ...filterCondition,
-      category
+    // add category filter
+    if (category && category !== 'all') {
+      filterCondition = {
+        ...filterCondition,
+        category
+      }
     }
-  }
-
-  return Record.find(filterCondition)
-    .sort({ date: 'asc' })
-    .lean()
-    .then(records => {
-      records.forEach(record => {
-        let date = new Date(record.date);
-        monthOptions.add(date.getFullYear().toString() + "-" + (date.getMonth() + 1).toString())
-        let category = record.category
-        categoryOptions.add(category)
-      })
-
-      return res.render('index', { records, monthOptions, categoryOptions, category, yearMonth })
+    const records = await Record.find(filterCondition)
+      .sort({ date: 'asc' })
+      .lean()
+    await records.forEach(record => {
+      const date = new Date(record.date);
+      monthOptions.add(date.getFullYear().toString() + "-" + (date.getMonth() + 1).toString())
+      const category = record.category
+      categoryOptions.add(category)
     })
-    .catch(error => console.log(error))
+    return res.render('index', { records, monthOptions, categoryOptions, category, date })
+    }
+    catch (error) {
+      console.log(error)
+    }
 })
 
 module.exports = router
