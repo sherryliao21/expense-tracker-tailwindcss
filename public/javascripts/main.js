2a1b86bc6297c9812fa464fe9f6a7f0b3926eb69
sherryliao21 <sherry.c.liao@gmail.com>
2021-06-24 12:49:13 +0800

    feat: delete deprecated feature & modify confirm message for deletion

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,14 +1,4 @@
 function confirmDelete() {
-  if (confirm('確定要刪除這間餐廳？')) return true
+  if (confirm('確定要刪除這筆記錄？')) return true
   return false
 }
-
-const resetBtn = document.querySelector('.reset-btn')
-const monthForm = document.querySelector('.form-select-month')
-const categoryForm = document.querySelector('.form-category')
-resetBtn.addEventListener('click', (e) => {
-  if (e.target === resetBtn) {
-    categoryForm.value = ''
-    monthForm.value = ''
-  }
-})
