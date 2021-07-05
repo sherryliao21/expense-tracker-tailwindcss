7d617e6c65a75d411b2df4d0edb4afcaecb73d48
sherryliao21 <sherry.c.liao@gmail.com>
2021-07-06 01:16:46 +0800

    feat: clean up deprecated code use template engine to render total amount instead of using DOM

--- public/javascripts/utility.js
+++ public/javascripts/utility.js
@@ -1,19 +1,10 @@
-function displayTotal() {
-  const totalAmount = document.querySelector('#total-amount-display')
-  const individualAmount = document.querySelectorAll('.individual-amount')
-  individualAmount.forEach(item => {
-    let total = Number(totalAmount.innerText)
-    total += Number(item.innerText)
-    return totalAmount.innerText = total
-  })
-}
 function checkForm() {
   const inputs = document.querySelectorAll('.form-control')
   const selection = document.querySelector('.form-select')
   let checkInputs = ''
-  inputs.forEach(input => {
+  inputs.forEach((input) => {
     if (!input.value || input.value === '0') {
-      return checkInputs = false
+      return (checkInputs = false)
     }
   })
   if (checkInputs === false || selection.value === '請選擇') {
@@ -24,10 +15,3 @@ function checkForm() {
 
 const submitNew = document.querySelector('.submit-new-btn')
 const container = document.querySelector('.container-new')
-
-displayTotal()
-// container.addEventListener('click', function onButtonClicked(event) {
-//   if (event.target.matches('.submit-new-btn')) {
-//     checkForm()
-//   }
-// })
