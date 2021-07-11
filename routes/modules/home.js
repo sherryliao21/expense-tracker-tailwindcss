5603835159e5f2d9d62ccf6d784c0687fe18bff5
sherryliao21 <sherry.c.liao@gmail.com>
2021-07-11 22:34:42 +0800

    fix: fix record filter getting wrong date issue

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,113 +1,113 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
-const Record = require('../../models/record')
+const Record = require("../../models/record")
 
-router.get('/', async (req, res) => {
-  try {
-    const userId = req.user._id
-    const records = await Record.find({ userId }).sort({ date: 'desc' }).lean()
-    let totalAmount = 0
-    const monthOptions = new Set()
-    const categoryOptions = new Set()
-    const type = 'total'
+router.get("/", async (req, res) => {
+	try {
+		const userId = req.user._id
+		const records = await Record.find({ userId }).sort({ date: "desc" }).lean()
+		let totalAmount = 0
+		const monthOptions = new Set()
+		const categoryOptions = new Set()
+		const type = "total"
 
-    await records.forEach((record) => {
-      const date = new Date(record.date)
-      monthOptions.add(
-        date.getFullYear().toString() + '-' + (date.getMonth() + 1).toString()
-      )
-      const category = record.category
-      categoryOptions.add(category)
+		await records.forEach(record => {
+			const date = new Date(record.date)
+			monthOptions.add(
+				date.getFullYear().toString() + "-" + (date.getMonth() + 1).toString()
+			)
+			const category = record.category
+			categoryOptions.add(category)
 
-      if (record.recordType === 'income') {
-        totalAmount += record.amount
-      } else {
-        totalAmount -= record.amount
-      }
-    })
+			if (record.recordType === "income") {
+				totalAmount += record.amount
+			} else {
+				totalAmount -= record.amount
+			}
+		})
 
-    return res.render('index', {
-      records,
-      monthOptions,
-      categoryOptions,
-      type,
-      totalAmount,
-    })
-  } catch (error) {
-    console.log(error)
-  }
+		return res.render("index", {
+			records,
+			monthOptions,
+			categoryOptions,
+			type,
+			totalAmount,
+		})
+	} catch (error) {
+		console.log(error)
+	}
 })
 
-router.get('/filter', async (req, res) => {
-  try {
-    const userId = req.user._id
-    const { category, date, type } = req.query
-    const monthOptions = new Set()
-    const categoryOptions = new Set()
+router.get("/filter", async (req, res) => {
+	try {
+		const userId = req.user._id
+		const { category, date, type } = req.query
+		const monthOptions = new Set()
+		const categoryOptions = new Set()
 
-    // create filterCondition object to store query filters
-    let filterCondition = { userId } // first pass in userId
+		// create filterCondition object to store query filters
+		let filterCondition = { userId } // first pass in userId
 
-    // add type filter
-    if (type && type !== 'total') {
-      filterCondition = {
-        ...filterCondition,
-        recordType: type,
-      }
-    }
-    // add month filter
-    if (date && date !== 'all') {
-      const [year, month] = date.split('-')
-      const startDate = new Date(year, month - 1)
-      const endDate = new Date(year, month, 0)
-      filterCondition = {
-        ...filterCondition,
-        date: {
-          $gte: startDate.toISOString().split('T')[0],
-          $lt: endDate.toISOString().split('T')[0],
-        },
-      }
-    }
-    // add category filter
-    if (category && category !== 'all') {
-      filterCondition = {
-        ...filterCondition,
-        category,
-      }
-    }
-    const records = await Record.find(filterCondition)
-      .sort({ date: 'desc' })
-      .lean()
+		// add type filter
+		if (type && type !== "total") {
+			filterCondition = {
+				...filterCondition,
+				recordType: type,
+			}
+		}
+		// add month filter
+		if (date && date !== "all") {
+			const [year, month] = date.split("-")
+			const startDate = new Date(year, month - 1, 2)
+			const endDate = new Date(year, month, 2)
+			filterCondition = {
+				...filterCondition,
+				date: {
+					$gte: startDate.toISOString().split("T")[0],
+					$lt: endDate.toISOString().split("T")[0],
+				},
+			}
+		}
+		// add category filter
+		if (category && category !== "all") {
+			filterCondition = {
+				...filterCondition,
+				category,
+			}
+		}
+		const records = await Record.find(filterCondition)
+			.sort({ date: "desc" })
+			.lean()
 
-    let totalAmount = 0
+		let totalAmount = 0
 
-    await records.forEach((record) => {
-      const date = new Date(record.date)
-      monthOptions.add(
-        date.getFullYear().toString() + '-' + (date.getMonth() + 1).toString()
-      )
-      const category = record.category
-      categoryOptions.add(category)
+		await records.forEach(record => {
+			const date = new Date(record.date)
+			monthOptions.add(
+				date.getFullYear().toString() + "-" + (date.getMonth() + 1).toString()
+			)
+			const category = record.category
+			categoryOptions.add(category)
 
-      if (record.recordType === 'income') {
-        totalAmount += record.amount
-      } else {
-        totalAmount -= record.amount
-      }
-    })
+			if (record.recordType === "income") {
+				totalAmount += record.amount
+			} else {
+				totalAmount -= record.amount
+			}
+		})
 
-    return res.render('index', {
-      records,
-      monthOptions,
-      categoryOptions,
-      category,
-      date,
-      type,
-      totalAmount,
-    })
-  } catch (error) {
-    console.log(error)
-  }
+		return res.render("index", {
+			records,
+			monthOptions,
+			categoryOptions,
+			category,
+			date,
+			type,
+			totalAmount,
+		})
+	} catch (error) {
+		console.log(error)
+	}
 })
 
 module.exports = router
