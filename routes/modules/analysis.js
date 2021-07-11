b950aaff5777b5bd6b2934827ba2fa491d26685c
sherryliao21 <sherry.c.liao@gmail.com>
2021-07-11 21:27:59 +0800

    install chart.js & setup analysis page

--- routes/modules/analysis.js
+++ routes/modules/analysis.js
@@ -1,9 +1,42 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
-const passport = require('passport')
+const passport = require("passport")
+const Chart = require("chart.js")
+const Record = require("../../models/record")
 
-router.get('/', async (req, res) => {
-  return res.render('analysis')
+router.get("/", async (req, res) => {
+	try {
+		const userId = req.user._id
+		const records = await Record.find({ userId }).sort({ date: "desc" }).lean()
+		let totalAmount = 0
+		const monthOptions = new Set()
+		const categoryOptions = new Set()
+		const type = "total"
+
+		await records.forEach(record => {
+			const date = new Date(record.date)
+			monthOptions.add(
+				date.getFullYear().toString() + "-" + (date.getMonth() + 1).toString()
+			)
+			const category = record.category
+			categoryOptions.add(category)
+
+			if (record.recordType === "income") {
+				totalAmount += record.amount
+			} else {
+				totalAmount -= record.amount
+			}
+		})
+
+		return res.render("analysis", {
+			records,
+			monthOptions,
+			categoryOptions,
+			type,
+		})
+	} catch (error) {
+		console.log(error)
+	}
 })
 
 module.exports = router
